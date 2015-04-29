b1e842548d7bd9e2c51aea9f749ce3a5c1882736
Andrew Hahn <andrewhahn@Andrews-MacBook-Pro.local>
2015-04-29 08:13:19 -0500

    Added randomItem class method

--- Homepwner/HAHNItem.h
+++ Homepwner/HAHNItem.h
@@ -16,6 +16,8 @@
     NSDate *_dateCreated;
 }
 
++ (instancetype)randomItem;
+
 // Designated initializer for HAHNItem
 - (instancetype)initWithItemName:(NSString *)name
                   valueInDollars:(int)value
